3681522632bae769b2cd1228d46e6fe016c510a8
Floréal Toumikian <floreal@gamned.com>
2014-06-02 17:52:42 +0200

    Added command create-article

---
+++ commands/create-article.rb
@@ -0,0 +1,53 @@
+usage       'create-article <title>'
+aliases     :ca
+summary     'Create a new article with the current date'
+description 'Create a new article with the current date. It will be generated in content/articles/[year]/[month]/[day]/[slug].md, slug will be generated from title'
+
+required :c, :vcs, 'specify the VCS to use'
+
+class CreateArticle < ::Nanoc::CLI::CommandRunner
+  def slugize title
+    title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  def run
+    args = arguments
+    if args.empty? then
+      raise Nanoc::Errors::GenericTrivial, "usage: #{command.usage}"
+    end
+
+    # Make sure we are in a nanoc site directory
+    require_site
+
+    # Set VCS if possible
+    set_vcs(options[:vcs])
+
+    title = args.join(' ')
+    slug = slugize title
+
+    now = Time.now
+
+    identifier = "articles/#{now.strftime '%Y/%m/%d'}/#{slug}".cleaned_identifier
+
+    file_path = "#{identifier}.md"
+
+    Nanoc::NotificationCenter.on(:file_created) do |file_path|
+      Nanoc::CLI::Logger.instance.file(:high, :create, file_path)
+    end
+
+    # Create item
+    data_source = site.data_sources[0]
+    data_source.create_item(
+      "Hi, I'm a new article!\n",
+      { :title => title, 
+        :created_at => "#{now.strftime '%F'}",
+        :author => ENV['USER'] },
+      identifier,
+      :extension => ".md"
+    )
+	  
+    puts "An item has been created at #{identifier}."
+  end
+end
+
+runner CreateArticle
