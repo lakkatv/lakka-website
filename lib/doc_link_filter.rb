cd63a0c408da2d57b8583cb5389ec49f0f186b0f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-12-26 10:36:53 +0100

    Fix irc links in doc filter

--- lib/doc_link_filter.rb
+++ lib/doc_link_filter.rb
@@ -4,7 +4,7 @@ class DocLinksFilter < Nanoc::Filter
 
   def run(content, params={})
     filtered = "#{content}"
-    needles = filtered.scan /href="[^http][^"]*"/
+    needles = filtered.scan /href="[^http|irc][^"]*"/
     needles.each do |needle|
       replacement = needle.gsub(' ', '%20').sub '="', '="/doc/'
       filtered.sub! needle, replacement
